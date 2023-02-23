a27a771aa603cd168f0e77f11f8656b30ffd2ee3
Noran Diaconu <norandiaconu@gmail.com>
2023-02-23 17:51:41 -0500

    Updated test

---
+++ src/app/typescript-basics/typescript-basics.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TypescriptBasicsComponent } from "./typescript-basics.component";
+
+describe("TypescriptBasicsComponent", () => {
+  let component: TypescriptBasicsComponent;
+  let fixture: ComponentFixture<TypescriptBasicsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TypescriptBasicsComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TypescriptBasicsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should multiplier", () => {
+    spyOn(window.console, "log");
+    component.multiplier();
+    expect(window.console.log).toHaveBeenCalledWith(50);
+    expect(window.console.log).toHaveBeenCalledWith(25);
+  });
+});
